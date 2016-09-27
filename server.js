e2f8f149aa1a1425bbbdd667257cddcfb02010c4
Stefan Szarek <stefan.szarek@grandparade.co.uk>
2016-09-27 19:57:12 +0200

    Getting pull request data

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 
 const restify = require('restify');
 const configuration = require('./config.js');
-const bitbucket = require('./src/bitbucket.js');
+const bitbucket = require('./src/bitbucket.js')(configuration.BITBUCKET_CREDENTIALS, configuration.REPOSITORIES);
 
 const server = restify.createServer();
 server.get('/repositories', bitbucket.getRepositories);
 server.get('/pull-requests', bitbucket.getPullRequests);
 
 server.listen(configuration.PORT, () => {
-    console.log(`Server is listening on port ${configuration.PORT}`);
+    console.log(`🌎 Server is listening on port ${configuration.PORT}`);
 });
--- src/bitbucket.js
+++ src/bitbucket.js
@@ -1,11 +1,54 @@
 'use strict';
 
-module.exports.getRepositories = function getRepositories(req, res, next) {
-    res.send('get repositories');
-    return next();
-};
+const request = require('request');
+const async = require('async');
 
-module.exports.getPullRequests = function getPullRequests(req, res, next) {
-    res.send('get pull requests');
-    return next();
+module.exports = function bitbucketFactory(credentials, repositories) {
+    const authHeader = 'Basic ' + new Buffer(`${credentials.LOGIN}:${credentials.PASSWORD}`).toString('base64');
+    repositories = repositories || [];
+
+    return {
+        getRepositories(req, res, next) {
+            res.send(repositories);
+            return next();
+        },
+
+        getPullRequests(req, res, next) {
+            getRepositoryPullRequests(repositories[0], function(err, r, body) {
+                if (err) {
+                    return next(err);
+                }
+
+                let json = null;
+                try {
+                    json = JSON.parse(body);
+                } catch (err) {
+                    return next(err);
+                }
+
+                const response = json.values.map(function(pullRequest) {
+                    return {
+                        title: pullRequest.title,
+                        link: pullRequest.links.html.href
+                    };
+                });
+                res.send(response);
+                return next();        
+            });
+        }
+    };
+
+    function getRepositoryPullRequests(repository, callback) {
+        makeRequest(`https://bitbucket.org/api/2.0/repositories/${repository.owner}/${repository.name}/pullrequests?state=OPEN`, callback);
+    }
+
+    function makeRequest(url, callback) {
+        return request({
+            url: url,
+            method: 'GET',
+            headers: {
+                'Authorization': authHeader
+            }
+        }, callback);
+    }
 };
